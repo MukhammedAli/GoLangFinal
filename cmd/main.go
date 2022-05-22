2b5e8b59cd993ebe2a25e9e9dbc27cc47741fa79
MukhammedAli <190103412@stu.sdu.edu.kz>
2022-05-22 18:43:38 +0600

    Adding necessary configurations

--- cmd/main.go
+++ cmd/main.go
@@ -7,16 +7,26 @@ import (
 	"github.com/MukhammedAli/GoFinalProject/pkg/handler"
 	"github.com/MukhammedAli/GoFinalProject/pkg/repository"
 	"github.com/MukhammedAli/GoFinalProject/pkg/service"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	if err := initConfig(); err != nil {
+		log.Fatalf("error initializing configs: %s", err.Error())
+	}
 	repos := repository.NewRepository()
 	services := service.NewService(repos)
 	handlers := handler.NewHandler(services)
 	//handlers := new(handler.Handler)
 
 	srv := new(todo.Server)
-	if err := srv.Run("8000", handlers.InitRoutes()); err != nil {
+	if err := srv.Run(viper.GetString("8000"), handlers.InitRoutes()); err != nil {
 		log.Fatalf("error occured while http server: %s", err.Error())
 	}
+}
+
+func initConfig() error {
+	viper.AddConfigPath("configs")
+	viper.SetConfigName("config")
+	return viper.ReadInConfig()
 }
