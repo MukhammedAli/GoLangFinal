6615e21349f78a5737350ff59aff77db4c29810d
MukhammedAli <190103412@stu.sdu.edu.kz>
2022-05-23 20:30:49 +0600

    Finalizing logic of API

--- pkg/repository/postgres.go
+++ pkg/repository/postgres.go
@@ -11,7 +11,7 @@ const (
 	todoListsTable  = "todo_lists"
 	usersListsTable = "users_lists"
 	todoItemsTable  = "todo_items"
-	listItemsTable  = "list_items"
+	listsItemsTable = "list_items"
 )
 
 type Config struct {
